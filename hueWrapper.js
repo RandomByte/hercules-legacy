d7ef7ed6f4dd0fc32fd16590043eb227e9bf5ff6
RandomByte <r@ndombyte.net>
2016-09-04 23:18:27 +0200

    Add wrapper for hue object

---
+++ hueWrapper.js
@@ -0,0 +1,126 @@
+var Hue = require("./hue.js");
+
+function HueWrapper() {
+	this.oHue = new Hue();
+	this.mHandledLights = {};
+	this.mHandledGroups = {};
+}
+
+HueWrapper.prototype = {
+	getReady: function() {
+		return this.oHue.getReady();
+	},
+
+	handleGroup: function(sRoom, bOn) {
+		var iGroupId, oGroup,
+			that = this;
+
+		// Get group ID for room
+		oGroup = this.oHue.getGroupByName(sRoom);
+		iGroupId = oGroup.sId;
+
+		// Load group to
+		//	a) check if we are allowed to handle it
+		//	b) change it's properties to save them back
+		return this.oHue.getClient().groups.getById(iGroupId).then(function(oGroup) {
+			var iLightId, oMetaGroup, i;
+
+			oMetaGroup = that.mHandledGroups[iGroupId];
+
+			if (oMetaGroup && oMetaGroup.oOffTimeout) {
+				// Clear any existing off-timeout first
+				clearTimeout(oMetaGroup.oOffTimeout);
+				oMetaGroup.oOffTimeout = null;
+			}
+
+			if (oGroup.anyOn && !oMetaGroup) {
+				// We won't handle this group
+				// -> fallback to lights
+				for (i = oGroup.lightIds.length - 1; i >= 0; i--) {
+					iLightId = oGroup.lightIds[i];
+					that.handleLight(iLightId, bOn);
+				}
+				return;
+			} else if (oMetaGroup && ((bOn && oGroup.allOn) || (!bOn && !oGroup.allOn))) {
+				// We handle it and it's already on or off
+				// -> nothing to do
+				// If we wouldn't handle it, we might want to set our color
+				return;
+			}
+
+			if (!oMetaGroup) {
+				oMetaGroup = that.mHandledGroups[iGroupId] = {};
+			}
+
+			oGroup.on = bOn;
+			if (bOn) {
+				console.log(new Date() + " Turning on group " + oGroup.name);
+				oGroup.brightness = 184;
+				oGroup.hue = 8411;
+				oGroup.saturation = 140;
+				return that.oHue.getClient().groups.save(oGroup);
+			}
+			oMetaGroup.oOffTimeout = setTimeout(function() {
+				console.log(new Date() + " Turning off group " + oGroup.name);
+				that.oHue.getClient().groups.save(oGroup);
+				that.mHandledGroups[iGroupId] = null;
+			}, 5000);
+		})
+		.catch(function(err) {
+			console.log(err);
+			console.log(new Date() + " Failed to update group");
+		});
+	},
+
+	handleLight: function(iLightId, bOn) {
+		var that = this;
+		// Load light to
+		//	a) check if we are allowed to handle it
+		//	b) change it's properties to save them back
+		return this.oHue.getClient().lights.getById(iLightId).then(function(oLight) {
+			var oMetaLight;
+
+			oMetaLight = that.mHandledLights[iLightId];
+
+			if (oMetaLight && oMetaLight.oOffTimeout) {
+				// Clear any existing off-timeout first
+				clearTimeout(oMetaLight.oOffTimeout);
+				oMetaLight.oOffTimeout = null;
+			}
+
+			if (oLight.on && !oMetaLight) {
+				// We won't handle this light
+				return;
+			} else if (oMetaLight && ((bOn && oLight.on) || (!bOn && !oLight.on))) {
+				// We handle it and it's already on or off
+				// -> nothing to do
+				// If we wouldn't handle it, we might want to set our color
+				return;
+			}
+
+			if (!oMetaLight) {
+				oMetaLight = that.mHandledLights[iLightId] = {};
+			}
+
+			oLight.on = bOn;
+			if (bOn) {
+				console.log(new Date() + " Turning on light " + oLight.name);
+				oLight.brightness = 184;
+				oLight.hue = 8411;
+				oLight.saturation = 140;
+				return that.oHue.getClient().lights.save(oLight);
+			}
+			oMetaLight.oOffTimeout = setTimeout(function() {
+				console.log(new Date() + " Turning off light " + oLight.name);
+				that.oHue.getClient().lights.save(oLight);
+				that.mHandledLights[iLightId] = null;
+			}, 5000);
+		})
+		.catch(function(err) {
+			console.log(err);
+			console.log(new Date() + " Failed to update light");
+		});
+	}
+};
+
+module.exports = HueWrapper;
--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 var oConfig = require("./config.json"),
 	Site = require("./site.js"),
-	Hue = require("./hue.js"),
+	HueWrapper = require("./hueWrapper.js"),
 	MqttClient = require("./mqtt.js"),
 
-	oHue,
-	mSites = {},
-	mHandledLights = {},
-	mHandledGroups = {};
+	oHueWrapper,
+	mSites = {};
 
 if (!oConfig.brokerUrl || !oConfig.topics) {
 	console.log("There's something missing in your config.json, please refer to config.example.json for an example");
 	process.exit(1);
 }
 
-oHue = new Hue();
-oHue.getReady()
+oHueWrapper = new HueWrapper();
+
+oHueWrapper.getReady()
 	.then(function() {
 		var oMqtt;
 
@@ -64,8 +63,7 @@ function handleStateChange(oStateChange) {
 }
 
 function handleRoom(oRoom) {
-	var bMotion, sRoom, sHueRoom,
-		oGroup;
+	var bMotion, sRoom, sHueRoom;
 
 	sRoom = oRoom.getName();
 
@@ -78,109 +76,6 @@ function handleRoom(oRoom) {
 	if (!bMotion && oRoom.isOccupied()) {
 		return; // Do nothing
 	}
-	oGroup = oHue.getGroupByName(sHueRoom);
-	handleGroup(oGroup.sId, bMotion);
-}
 
-function handleGroup(iGroupId, bOn) {
-	// Load group to
-	//	a) check if we are allowed to handle it
-	//	b) change it's properties to save them back
-	return oHue.getClient().groups.getById(iGroupId).then(function(oGroup) {
-		var iLightId, oMetaGroup, i;
-
-		oMetaGroup = mHandledGroups[iGroupId];
-
-		if (oMetaGroup && oMetaGroup.oOffTimeout) {
-			// Clear any existing off-timeout first
-			clearTimeout(oMetaGroup.oOffTimeout);
-			oMetaGroup.oOffTimeout = null;
-		}
-
-		if (oGroup.anyOn && !oMetaGroup) {
-			// We won't handle this group
-			// -> fallback to lights
-			for (i = oGroup.lightIds.length - 1; i >= 0; i--) {
-				iLightId = oGroup.lightIds[i];
-				handleLight(iLightId, bOn);
-			}
-			return;
-		} else if (oMetaGroup && ((bOn && oGroup.allOn) || (!bOn && !oGroup.allOn))) {
-			// We handle it and it's already on or off
-			// -> nothing to do
-			// If we wouldn't handle it, we might want to set our color
-			return;
-		}
-
-		if (!oMetaGroup) {
-			oMetaGroup = mHandledGroups[iGroupId] = {};
-		}
-
-		oGroup.on = bOn;
-		if (bOn) {
-			console.log(new Date() + " Turning on group " + oGroup.name);
-			oGroup.brightness = 184;
-			oGroup.hue = 8411;
-			oGroup.saturation = 140;
-			return oHue.getClient().groups.save(oGroup);
-		}
-		oMetaGroup.oOffTimeout = setTimeout(function() {
-			console.log(new Date() + " Turning off group " + oGroup.name);
-			oHue.getClient().groups.save(oGroup);
-			mHandledGroups[iGroupId] = null;
-		}, 5000);
-	})
-	.catch(function(err) {
-		console.log(err);
-		console.log(new Date() + " Failed to update group");
-	});
-}
-
-function handleLight(iLightId, bOn) {
-	// Load light to
-	//	a) check if we are allowed to handle it
-	//	b) change it's properties to save them back
-	return oHue.getClient().lights.getById(iLightId).then(function(oLight) {
-		var oMetaLight;
-
-		oMetaLight = mHandledLights[iLightId];
-
-		if (oMetaLight && oMetaLight.oOffTimeout) {
-			// Clear any existing off-timeout first
-			clearTimeout(oMetaLight.oOffTimeout);
-			oMetaLight.oOffTimeout = null;
-		}
-
-		if (oLight.on && !oMetaLight) {
-			// We won't handle this light
-			return;
-		} else if (oMetaLight && ((bOn && oLight.on) || (!bOn && !oLight.on))) {
-			// We handle it and it's already on or off
-			// -> nothing to do
-			// If we wouldn't handle it, we might want to set our color
-			return;
-		}
-
-		if (!oMetaLight) {
-			oMetaLight = mHandledLights[iLightId] = {};
-		}
-
-		oLight.on = bOn;
-		if (bOn) {
-			console.log(new Date() + " Turning on light " + oLight.name);
-			oLight.brightness = 184;
-			oLight.hue = 8411;
-			oLight.saturation = 140;
-			return oHue.getClient().lights.save(oLight);
-		}
-		oMetaLight.oOffTimeout = setTimeout(function() {
-			console.log(new Date() + " Turning off light " + oLight.name);
-			oHue.getClient().lights.save(oLight);
-			mHandledLights[iLightId] = null;
-		}, 5000);
-	})
-	.catch(function(err) {
-		console.log(err);
-		console.log(new Date() + " Failed to update light");
-	});
+	oHueWrapper.handleGroup(sHueRoom, bMotion);
 }
